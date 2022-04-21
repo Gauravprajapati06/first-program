8fae06008f30adee1af1ad40f417fb16a3d55f1b
Gauravprajapati06 <103199717+Gauravprajapati06@users.noreply.github.com>
2022-04-21 01:40:53 -0700

    Create first.c

---
+++ first.c
@@ -0,0 +1,12 @@
+/*program to add two numbers*/
+
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("enter 2 numbers:");
+    scanf("%d %d",&num1,&num2);
+    sum=num1+num2;
+    printf("the sum of the 2 numbers is  %d",sum);
+    return 0;
+}
